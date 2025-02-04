98e00c56b3a09d6bc4b986e4f3e1303b8398115b
Mert Cobanov <mertcobanov@gmail.com>
2025-02-04 10:19:20 +0300

    Add database utility functions for table management and search

---
+++ db_utils.py
@@ -0,0 +1,38 @@
+import lancedb
+import pyarrow as pa
+
+
+def get_lancedb_client(uri, token=None):
+    if token is None:
+        return lancedb.connect(uri)
+    else:
+        return lancedb.connect(uri, token=token)
+
+
+def create_table(db, table_name, dim=2048):
+    """Creates a table in the database if it does not exist. Raises an error if the table already exists."""
+    if table_name in db.table_names():
+        raise ValueError(f"Table '{table_name}' already exists. Creation aborted.")
+
+    print(f"Creating table {table_name}")
+    schema = pa.schema(
+        [
+            pa.field("vector", pa.list_(pa.float32(), dim)),
+            pa.field("filepath", pa.string()),
+        ]
+    )
+
+    return db.create_table(table_name, schema=schema)
+
+
+def open_table(db, table_name):
+    """Opens an existing table in the database. Raises an error if the table does not exist."""
+    if table_name not in db.table_names():
+        raise ValueError(f"Table '{table_name}' does not exist.")
+
+    print(f"Opening table {table_name}")
+    return db.open_table(table_name)
+
+
+def get_search_results(table, query):
+    return table.search(query)
